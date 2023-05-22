af400cc9a1ca525c347bba34435314df6fd34167
iRommero <irommero@protonmail.com>
2023-05-21 20:29:18 -0700

    Update view controllers

---
+++ examen2p411/VCWelcomeUsuario.swift
@@ -0,0 +1,24 @@
+//
+//  VCWelcomeUsuario.swift
+//  examen2p411
+//
+//  Created by Diego Romero on 21/05/23.
+//
+
+import Cocoa
+
+class VCWelcomeUsuario: NSViewController {
+
+    //Labels
+    @IBOutlet weak var lblUsuario: NSTextField!
+    
+    //Buttons
+    @IBOutlet weak var btnAlta: NSButton!
+    @IBOutlet weak var btnConsulta: NSButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
